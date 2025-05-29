ce082c8260ec994983edbe36000e31c226c1d446
gavin-c-l <landrg@rpi.edu>
2025-05-29 10:02:33 -0400

    hi

--- arbitrary.java
+++ arbitrary.java
@@ -1,7 +1,7 @@
 public class Main { // Class name should match the filename (Main.java)
     public static void main(String[] args) { // Entry point of the program
         // Your code goes here
-        System.out.println("Hello, World!"); // Example line of code
+        System.out.println("Hello, Cole!"); // Example line of code
         System.out.println("Hello Yippee!!");
     }
 }
